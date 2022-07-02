5214d489eb4d9a215db89447ace18778f25377f3
karadenizemirr <karadenizemirr@gmail.com>
2022-07-02 23:03:40 +0300

    master

---
+++ ardahanfinal_server/src/admin/app.js
@@ -0,0 +1,25 @@
+export default {
+  config: {
+    theme: {
+      colors: {
+
+      },
+    },
+    locales: ['tr'],
+    translations: {
+      tr: {
+        User: "Kullanıcılar",
+        Contact: "İletişim",
+        Application: "Ön Kayıt",
+        Comment: "Yorumlar",
+        Achievement: "Başarılarımız"
+      }
+    },
+    // Disable video tutorials
+    tutorials: false,
+    // Disable notifications about new Strapi releases
+    notifications: { release: false },
+  },
+
+  bootstrap() {},
+};
